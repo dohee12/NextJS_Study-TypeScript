622cd8df95ab32b52be44396143a34d81308177d
dohee12 <happyddo42@gmail.com>
2025-03-26 23:36:58 +0900

    feat: Table Components, CVA Button 추가

--- src/app/buttons/page.tsx
+++ src/app/buttons/page.tsx
@@ -1,13 +1,47 @@
 import { Bell } from "lucide-react";
 import Button from "../../components/Button";
 import CvaButton from "@/components/CvaButton";
+import Table from "@/components/Table";
 
 export default function Home() {
+  const headers = [
+    { key: "fullName", text: "Full Name" },
+    { key: "email", text: "Email" },
+    { key: "group", text: "Group" },
+  ];
+
+  const items = [
+    {
+      fullName: "Danilo Sousa",
+      email: "danilo@example.com",
+      group: "Developer",
+    },
+    {
+      fullName: "Zahra Ambessa",
+      email: "zahra@example.com",
+      group: "Admin",
+    },
+    {
+      fullName: "Jasper Eriksson",
+      email: "jasper@example.com",
+      group: "Developer",
+    },
+  ];
+
   return (
     <div className="p-8 space-y-2">
+      <div className="p-8">
+        <div>Table Components</div>
+        <Table variant="ghost" headers={headers} items={items} />
+        <div className="py-8">
+          <Table headers={headers} items={items} />
+        </div>
+      </div>
       <div className="py-8">
         <div> CVA Button </div>
-        <CvaButton />
+        <CvaButton variant="solid" />
+        <CvaButton variant="surface" />
+        <CvaButton variant="soft" size={3} />
       </div>
       <div className="">
         <Button variant="solid" size="4">
